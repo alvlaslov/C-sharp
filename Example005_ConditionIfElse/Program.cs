2e781d22ddf79a4f97c47c98e6ea7c04928ea937
Alex <you@example.com>
2022-07-09 12:48:11 +0300

    add Example005_Condition If-Elsem

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert name:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yeah, this is MASHA");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
